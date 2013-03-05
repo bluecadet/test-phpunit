8adb818d29fa92aa9e279a6711670744f74ed75f
ornj <steve@stevehurwitz.com>
2013-03-05 11:46:10 -0500

    Added more complex test with data provider

---
+++ www/src/Test/URLTest.php
@@ -0,0 +1,38 @@
+<?php
+    
+namespace src\Test;
+
+use src\URL;
+
+class URLTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test src\URL::sluggify()
+     *
+     * @param string $original  String to be sluggified
+     * @param string $expected  What the result should be
+     * 
+     * @dataProvider providerTestSluggifyReturnsSluggifiedString
+     */
+    public function testSluggifyReturnsSluggifiedString($original, $expected)
+    {
+        $url = new URL();
+        $result = $url->sluggify($original);
+        $this->assertEquals($expected, $result);
+    }
+    
+    /**
+     * Dataprovider for $this::testSluggifyReturnsSluggifiedString()
+     */
+     public function providerTestSluggifyReturnsSluggifiedString()
+     {
+         return array(
+             array('This string will be sluggified', 'this-string-will-be-sluggified'),
+             array('THIS STRING WILL BE SLUGGIFIED', 'this-string-will-be-sluggified'),
+             array('This1 string2 will3 be 44 sluggified10', 'this1-string2-will3-be-44-sluggified10'),
+             array('This! @string#$ %$will ()be "sluggified', 'this-string-will-be-sluggified'),
+             array("Tänk efter nu – förr'n vi föser dig bort", 'tank-efter-nu-forrn-vi-foser-dig-bort'),
+             array('', ''),
+         );
+     }
+}
